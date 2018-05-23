b1fde7c8496505dfd52c5a17859c0cd3706c2ccc
15pa1a0598eissah <15pa1a0598@vishnu.edu.in>
2018-05-23 11:13:04 +0530

    my second commit

--- file1.py
+++ file1.py
@@ -1,13 +1,15 @@
 import webbrowser
 
 
+# creating mymovie class
 class MyMovie():
-
+    # init function to initialize values
     def __init__(self, movie_title,
                  poster_image, trailer):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer
 
+    # show trailer function to open webpage from webbrowser.open method
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
