fc562e679d7d114c1d1e9572a4233ae08f3d1949
giriharish <15pa1a05g5@vishnu.edu.in>
2018-05-23 22:39:30 +0530

    my third commit

--- README.md
+++ README.md
@@ -20,5 +20,26 @@ PROCEDURE:
     and 'bit.ly' to shorten my url links
     5.At last I uploaded my files into git hub
 
+INSTALLATION PROCESS:
+    1.To run this application we need to install python 3.6.5 
+    2.The website link to download https://www.python.org/ftp/python/3.6.5/python-3.6.5.exe
+    3.To upload a project we need to create github account and install git
+    4.The link to download git is https://central.github.com/deployments/desktop/desktop/latest/win32
+
+HOW TO RUN:
+    1.create one folder then create cgi-bin sub folder inside of that folder
+    2.place python files in that folder
+    3.open command prompt there and run server outside of cgi-bin folder
+    4.To run server use the command python -m http.server --cgi 8888(port number)
+    5.Go to any webbrowser and type localhost:8888(localhost with port number)
+    4.Then cgi-bin and other folders are displayed
+    6.Click on cgi-bin and write file name in the url example: 'localhost:8888/cgi-bin/main.py'
+
+HOW TO UPLOAD A PROJECT:
+    1.create a folder and open command prompt by typing cmd in the path
+    2.The clone your repository in that folder by command 'git clone https://github.com/Eissah598/movietrailer.git'
+    4.paste your files in that folder and run command 'git add .'
+    5.Then push your master branch to git hub by command 'git push origin master'
+    
 RESULT:
     provides a great experience on fundamentals of html, css, js, python
--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-import file1
-import file2
+import media
+import my_movies
 
 
-# rabnebanadijodi title,poster image,urlid
-rnbz = file1.MyMovie("Rab Ne Banadi Zodi",
+''' movie title, posterlink, youtube url id
+    is sent to the MyMovie class in 'media' file
+'''
+
+rnbz = media.MyMovie("Rab Ne Banadi Zodi",
                      "https://bit.ly/2kf6Xhk",
                      "eBi8syxPd14")
-# raone title,poster image,urlid
-rone = file1.MyMovie("Ra-One",
+rone = media.MyMovie("Ra-One",
                      "https://bit.ly/2s6me7x",
                      "d9PlHc_qVKw")
-# chennai express title,poster image,urlid
-ce = file1.MyMovie("Chennai Express",
+ce = media.MyMovie("Chennai Express",
                    "https://bit.ly/2s6mh3d",
                    "rARol7Dk2zo")
-# zero title,poster image,urlid
-zero = file1.MyMovie("Zero",
+zero = media.MyMovie("Zero",
                      "https://bit.ly/2klHOBL",
                      "RF7DhGIQE1k")
-# mohabbatein title,poster image,urlid
-mohabbatein = file1.MyMovie("Mohabbatein",
+mohabbatein = media.MyMovie("Mohabbatein",
                             "https://bit.ly/2kfl9GS",
                             "OjlZFIY7VHU")
-# raees title,poster image,urlid
-raees = file1.MyMovie("Raees",
+raees = media.MyMovie("Raees",
                       "https://bit.ly/2LlsgtO",
                       "J7_1MU3gDk0")
 
 # in every part the Movie class in a1 file is called
 movies = [rnbz, rone, ce, zero, mohabbatein, raees]
 # the open_module function in a2 file is called
-file2.open_module(movies)
+my_movies.open_module(movies)
